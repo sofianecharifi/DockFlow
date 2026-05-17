c9f1bd4a83bd05573133ba264224e03a7891bd49
Sofiane Charifi Alaoui <sofiane.charifialaoui@gmail.com>
2026-05-17 21:51:55 +0200

    feat: implement container controller and update project dependencies

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const Dockerode = require("dockerode");
+
+const docker = new Dockerode();
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/api/images", (req, res) => {
+    docker.listImages({ all: true }, (err, images) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error listing images");
+        } else {
+            const mappedImages = images.map((image) => ({
+                id: image.Id,
+                names: image.RepoTags,
+            }));
+            res.json(mappedImages);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
